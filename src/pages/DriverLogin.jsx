742dac1d8d1badfba2415804918e1f314a547d8f
Chinmai Nimgade <chinmainimgade1608@gmail.com>
2023-10-06 00:22:58 +0530

    Update DriverLogin.jsx

--- src/pages/DriverLogin.jsx
+++ src/pages/DriverLogin.jsx
@@ -18,12 +18,15 @@ function DriverLogin() {
         <div class="flex justify-around ">  
           <div class="border border-cyan-600 w-1/2 rounded-2xl bg-cyan-50">
             <div class="text-3xl cursor-none font-serif font-bold m-5 text-center text-neutral-950 underline underline-offset-2"> Driver's Login </div>
-            <div class="cursor-text border border-cyan-600 bg-white h-8 w-2/3 rounded-full m-3 mx-auto"
-                   placeholder="Driver's Name">  </div>
-            <div class="cursor-text border border-cyan-600 bg-white h-8 w-2/3 rounded-full m-3 mx-auto"
-                   placeholder="Driver's Name">  </div>
+            <div class="cursor-text border border-cyan-600 bg-white h-8 w-2/3 rounded-full m-3 mx-auto">
+              <input class="w-4/5 my-1 mx-5" type="text" placeholder="Username"/></div>
+            <div class="cursor-text border border-cyan-600 bg-white h-8 w-2/3 rounded-full m-3 mx-auto">
+              <input class="w-4/5 my-1 mx-5" type="password" placeholder="Password"/></div>
             <div class="cursor-pointer font-serif font-bold text-center text-lg text-neutral-900 border border-cyan-600 bg-sky-400 h-8 w-1/6 rounded-full m-3 mx-auto"
                   > <Link to="/Driverhomepage">Login</Link> </div>
+            <div class="text-center cursor-pointer mb-3 hover:underline">
+              <sub class="hover text-center ">Don't have an account? let's create new one</sub>
+            </div>
 
           </div>
         </div>
