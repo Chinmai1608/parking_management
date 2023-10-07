8b3e2b96dcb2d5921774a6b052888975473b16c6
Chinmai Nimgade <chinmainimgade1608@gmail.com>
2023-10-07 16:17:02 +0530

    Update Driverhomepage.jsx

--- src/pages/Driverhomepage.jsx
+++ src/pages/Driverhomepage.jsx
@@ -9,7 +9,7 @@ function Driverhomepage(){
                alt="Saarthi Parking logo"/></Link>
             </div>
             <div class="grid grid-cols-2">
-                <div class=" h-96 bg-slate-600">
+                <div class=" h-96 ">
                     <div class="grid grid-cols-2">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d119182.2758375204!2d75.485787!3d21.0148284!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x3bd90fafffffffff%3A0x98d3058aff18773e!2s60%2C%20Mahatma%20Gandhi%20Rd%2C%20Jilha%20Peth%2C%20Pratap%20Nagar%2C%20Jalgaon%2C%20Maharashtra%20425001!3m2!1d21.0148482!2d75.5681887!5e0!3m2!1sen!2sin!4v1696255943797!5m2!1sen!2sin" 
                         class="m-5 border border-neutral-900 border-spacing-x-10" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
